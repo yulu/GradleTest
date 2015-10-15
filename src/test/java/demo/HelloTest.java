340b000e22f475d6a21d505d984fba61557f2c31
Yu Lu <yulu8798@gmail.com>
2015-10-15 22:27:52 +0800

    test a html to image renderer

--- src/test/java/demo/HelloTest.java
+++ src/test/java/demo/HelloTest.java
@@ -1,5 +1,6 @@
 package demo;
 
+import gui.ava.html.image.generator.HtmlImageGenerator;
 import junit.framework.TestCase;
 import org.junit.Test;
 
@@ -11,6 +12,12 @@ public class HelloTest extends TestCase {
   public void testSayHello() {
     Hello hello = new Hello();
     assertEquals("Hello, Gradle", hello.sayHello());
+
+    HtmlImageGenerator imageGenerator = new HtmlImageGenerator();
+    imageGenerator.loadHtml("<b>Hello World!</b> Please goto <a title=\"Goto Google\" href=\"http://www.google.com\">Google</a>." +
+      "<img src=\"http://www.dosavannah.com/sites/default/files/photos/cake.jpg\" height = \"42\" width=\"42\">");
+    imageGenerator.saveAsImage("hello-world.png");
+    imageGenerator.saveAsHtmlWithMap("hello-world.html", "hello-world.png");
   }
 
 }
